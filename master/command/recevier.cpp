acf6aac7b79aa5403fae5f253df077a1866e9c40
Winnerhust <910894904@qq.com>
2013-08-15 10:21:07 +0800

    Create recevier.cpp

---
+++ master/command/recevier.cpp
@@ -0,0 +1,16 @@
+#ifndef __COMRECEVIER_CPP__
+#define __COMRECEVIER_CPP__
+#include <iostream>
+#include "recevier.h"
+using namespace std;
+Recevier::Recevier()
+{
+}
+Recevier::~Recevier()
+{
+}
+void Recevier::Action()
+{
+        cout<<"Recevier::Action"<<endl;
+}
+#endif
