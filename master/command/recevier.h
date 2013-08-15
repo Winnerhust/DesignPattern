4257d253c7540e101ce1bd89e356ed563caebe45
Winnerhust <910894904@qq.com>
2013-08-15 10:20:42 +0800

    Create recevier.h

---
+++ master/command/recevier.h
@@ -0,0 +1,14 @@
+#ifndef __COMRECEVIER_H__
+#define __COMRECEVIER_H__
+
+#include "command.h"
+
+class Recevier
+{
+public:
+        Recevier();     
+        ~Recevier();
+        void Action();
+};
+
+#endi
